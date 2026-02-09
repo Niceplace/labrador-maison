07ea50bb87825cc4667fd07dc1955ce2b1827320
Simon Beaulieu <s.beaulie2@gmail.com>
2026-02-08 20:28:48 -0500

    Ensure client certs can be generated more than once for the same CN with unique serials

--- root-certificate-authority/generate-client-cert.sh
+++ root-certificate-authority/generate-client-cert.sh
@@ -3,21 +3,21 @@
 CURRENT_DIR=$(pwd)
 COMMON_NAME="*.thinkcenter.dev"
 
-KEY_PATH="/root/ca/intermediate/private/${COMMON_NAME}.key.pem"
-CERT_SIGNING_REQUEST_PATH="/root/ca/intermediate/csr/${COMMON_NAME}csr.pem"
-CERT_PATH="/root/ca/intermediate/certs/${COMMON_NAME}.cert.pem"
+KEY_PATH="/root/ca/intermediate/private/wildcard.client.key.pem"
+CERT_SIGNING_REQUEST_PATH="/root/ca/intermediate/csr/wildcard.client.csr.pem"
+CERT_PATH="/root/ca/intermediate/certs/wildcard.client.cert.pem"
 
 CNF_PATH="/root/ca/intermediate/openssl.cnf"
 CA_CHAIN_PATH="/root/ca/intermediate/certs/ca-chain.cert.pem"
 
-CERT_OUT="$2/${COMMON_NAME}.client.pem"
+CERT_OUT="$1/wildcard.client.pem"
 
 
 cd /root/ca
 # Generate key
 openssl genrsa -out "$KEY_PATH" 2048
 echo "Generated key in $KEY_PATH"
-cp -rf "$CA_CHAIN_PATH" "$2"
+cp -rf "$CA_CHAIN_PATH" "$1"
 echo "Copied the chain into the destination directory $2"
 
 # Generate certificate signing request
@@ -36,6 +36,7 @@ openssl ca -config "$CNF_PATH" \
        	-days 375 \
        	-notext \
 	-md sha256 \
+	-unique_serial \
        	-in "${CERT_SIGNING_REQUEST_PATH}" \
        	-out "$CERT_OUT"
 
